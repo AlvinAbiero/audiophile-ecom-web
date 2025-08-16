90ed0a39a616e846fd380b0e7ce3eb329b716343
Alvin Abiero <abieroalvin@gmail.com>
2025-08-16 21:41:58 +0300

    Update FooterLinks.tsx

--- src/components/links/FooterLinks.tsx
+++ src/components/links/FooterLinks.tsx
@@ -5,7 +5,7 @@ import { links } from "@/utils/links";
 const FooterLinks = () => {
   return (
     <div>
-      <ul className="flex md:space-x-8 sm:space-x-6 space-x-3">
+      <ul className="flex md:space-x-8 sm:space-x-6 space-x-2">
         {links.map((link) => (
           <li
             className="sm:text-[.8rem] text-[.75rem] tracking-[.125rem] text-white hover:text-gray-300 transform transition-color transition-delay-[0.2s] transition-linear font-bold uppercase"
